07fd4de5bf3da26db9ab7e1621c047505c35c502
ohnizbak <wlsgheowkd032@gmail.com>
2024-07-30 23:27:03 +0900

    ddddddd

---
+++ mac/js/carousel.js
@@ -0,0 +1,15 @@
+// glowplay recommendation
+
+document.addEventListener('DOMContentLoaded', ()=> {
+    const slides = document.querySelector('.recomend-slide');
+    const prevButton = document.querySelector('.recommend-button.prev');
+    const nextButton = document.querySelector('recommend-button.next');
+    const indicators = document.querySelector('indicator')
+    const carouselContainer = document.querySelector('.recommend-container')
+    let currenIndex = 0; 
+
+    function showSlide(index) {
+        const slideWidth = slide[0].clientWidth;
+        
+    }
+})
