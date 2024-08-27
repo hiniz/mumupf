d7f5a5e6cf2f83f66c8b143d5e5b9b6da36437a6
ohnizbak <wlsgheowkd032@gmail.com>
2024-08-27 22:41:25 +0900

    dd

--- c&c/js/button.js
+++ c&c/js/button.js
@@ -4,3 +4,44 @@ const hiddenButton = document.querySelector('.more-info-button')
         hiddenButton.addEventListener('click', ()=> {
             hiddenDiv.classList.toggle('hidden')
         })
+
+
+let upBtn = document.querySelector('#topBtn');
+
+const scroll = () => {
+    if (window.scrollY !== 0) {
+        setTimeout(() => {
+            window.scrollTo(0, window.scrollY - 50);
+            scroll();
+        }, 1);
+    }
+}
+
+upBtn.addEventListener('click', scroll);
+
+
+
+
+
+
+
+
+// shop
+document.addEventListener('DOMContentLoaded', function() {
+    const tabButton = document.querySelectorAll('.category-button')
+    const tabContents = document.querySelectorAll('.products-container')
+
+    tabButton.forEach(button => {
+        button.addEventListener('click', () => {
+            const tabId = button.getAttribute('data-tab');
+
+            tabContents.foreach(content => {
+                if (content.id === tabId) {
+                    content.classList.add('active')
+                }else {
+                    content.classList.remove('active')
+                }
+            })
+        })
+    })
+})
